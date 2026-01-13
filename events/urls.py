35387ab5a51f88eeb583a4aeb15b0b52c3ca7201
namramoradiya <moradiyanamra@gmail.com>
2026-01-13 16:23:02 +0530

    Login Page

---
+++ events/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.event_list, name='event_list'),
+    path('new/', views.event_create, name='event_create'),
+    path('edit/<int:id>/', views.event_update, name='event_update'),
+    path('delete/<int:id>/', views.event_delete, name='event_delete'),
+]
