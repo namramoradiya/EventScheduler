cfa4b230e1aa3c81800d0b2a6b8b3f1f9103612e
namramoradiya <moradiyanamra@gmail.com>
2026-01-13 17:44:28 +0530

    Description Adding

--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.event_list, name='event_list'),
     path('new/', views.event_create, name='event_create'),
-    path('edit/<int:id>/', views.event_update, name='event_update'),
+    path('edit/<int:id>/', views.event_update, name='updating'),
     path('delete/<int:id>/', views.event_delete, name='event_delete'),
     path('register/', views.register, name='register'),
 ]
