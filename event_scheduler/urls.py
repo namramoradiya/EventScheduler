db2863d7dd5bd52eff4d8ea63bf9448a74e4cef7
namramoradiya <moradiyanamra@gmail.com>
2026-01-13 16:59:01 +0530

    Logout Function

--- event_scheduler/urls.py
+++ event_scheduler/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/', include('events.urls')),
     path('', include('django.contrib.auth.urls')),
-    path('login/', include('django.contrib.auth.urls')),
     path('', lambda request: redirect('event_list')),
     
 ]
